18a00e236ee22e6f1fe40017cb176a3ef17b23ab
Дыня Орехов <danaran319@gmail.com>
2026-06-15 11:07:23 +0300

    trying to fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { Pool } from 'pg';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
     user: process.env.DB_USER || 'mynewuser',
